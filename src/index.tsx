e9b1dcdd82e0b77c81f5a7043ccad788c51ac186
flatlinediver <flatlinediver@protonmail.com>
2021-04-01 05:52:51 -0300

    Removed deprecated exported SpinnerContext

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React, { FC } from 'react';
-import { Store, StoreProvider, useStore } from './store';
+import { StoreProvider, useStore } from './store';
 import { Spinner as SpinnerSvg } from './presentation';
 import { SpinnerProps } from './types';
 import { CONSTANTS, ErrorBoundary } from './helpers';
 
 export const DEFAULT_SPINNER_VALUES = CONSTANTS.DEFAULT_PROPS;
-
-// Deprecated
-export const SpinnerContext = Store;
 
 export const SpinnerProvider = StoreProvider;
 
